657d6179e6bb5d8472fd147e20f08b0fe4625b37
Hazem-mak <hazem.mak95@gmail.com>
2021-01-07 13:35:04 +0200

    blue note is on

--- src/app/note-board/note-board.component.ts
+++ src/app/note-board/note-board.component.ts
@@ -1,19 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-note-board',
   templateUrl: './note-board.component.html',
   styleUrls: ['./note-board.component.scss']
+  
 })
+
 export class NoteBoardComponent implements OnInit {
  note: number[] = [47,75,103,131,159,187,215,9,9,9,0,6,4,8,9,5];
  notey: number[] = [47,75,103,131,159,187,215];
  notex: number =100;/* from 250 to 1050*/
+ yind:number[]=[1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6];
  f: number=90;
+ xblue:number=1;
+ yblue:number=1;
+ ynum:number[]=[];
+ id:any;
+ 
+ noteime:number =500;
+ boardtime:number=2000;
+ note_counter:number=16;
   constructor() { }
 
   ngOnInit(): void {
-   
+    for (let index = 0; index < this.note_counter+1; index++) {
+      this.ynum[index]=Math.floor(((6*10*Math.random()))/9);}
+     
+   this.rand_generat();
   }
-
+  rand_generat()
+  { 
+    
+    this.id=setInterval(()=>{
+    this.xblue=this.xblue+1;
+    this.yblue=this.ynum[this.xblue];
+    if (this.xblue>=this.note_counter) {
+      for (let index = 0; index < this.note_counter+1; index++) {
+        this.ynum[index]=Math.floor(((6*10*Math.random()))/9);    
+      }   
+      this.xblue=0; 
+      this.yblue=this.ynum[this.xblue];
+    }},this.noteime)
+  
+  }
 }
--- src/app/rand/rand.component.ts
+++ src/app/rand/rand.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-
 @Component({
   selector: 'app-rand',
   templateUrl: './rand.component.html',
@@ -7,19 +6,10 @@ import { Component, Input } from '@angular/core';
 })
 export class RandComponent  {
   @Input() xvalue :any;
-  @Input() yvalue :any;
-  @Input() xnum :any;
-  xblue:number=1;
-  noteime:number =200;
-  boardtime:number=2000;
-  note_counter:number=16;
- notey: number[] = [47,75,103,131,159,187,215];
+  @Input() yvalue :any; 
+  @Input() xblue:any
+  @Input() yblue: any; 
+ notey: number[] = [47,75,103,131,159,187,215];//must add 60
  notex: number =50;/* from 250 to 1050*/
- ynum:number=Math.floor(((6*10*Math.random()))/9);
- id=setInterval(()=>{this.xblue=this.xblue+1;if (this.xblue==this.note_counter) {
-  this.ynum=Math.floor(((6*10*Math.random()))/9);
-  this.xblue=0; 
- }},this.noteime)
-
-
+ 
 }
